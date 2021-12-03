9d02bf05ca3327d085a81ec37cdffc9bc7dc8647
Mario <yzarckwow@gmail.com>
2021-12-03 14:10:34 -0300

    update

--- projetoCoordendas.js
+++ projetoCoordendas.js
@@ -6,9 +6,11 @@
     const $btnSearch = doc.querySelector("[data-js='btnSearch']")
     var $table = doc.querySelector("[data-js='table']")
     var enterprise = []
+    var requestURL =
+      'https://raw.githubusercontent.com/Mario-Cesar-Yzarck/data/main/data.json'
 
     const ajax = new XMLHttpRequest()
-    ajax.open('GET', 'data.json')
+    ajax.open('GET', requestURL)
     ajax.send()
     var data
     eventReadyState()
@@ -49,38 +51,69 @@
     }
 
     function whatIsSelected(enterpriseSelected) {
+      enterprise = []
       switch (enterpriseSelected.value) {
         case '$101_NORTE':
-          enterprise = []
           enterprise.push(data.$101_NORTE)
           break
         case '$AEL_PARTICIPACOES':
-          enterprise = []
           enterprise.push(data.$AEL_PARTICIPACOES)
           break
         case '$AGRIPINO':
-          enterprise = []
           enterprise.push(data.$AGRIPINO)
           break
         case '$AGUA_DOCE_MINERACAO':
-          enterprise = []
           enterprise.push(data.$AGUA_DOCE_MINERACAO_1)
           enterprise.push(data.$AGUA_DOCE_MINERACAO_2)
           enterprise.push(data.$AGUA_DOCE_MINERACAO_3)
           enterprise.push(data.$AGUA_DOCE_MINERACAO_4)
           break
         case '$AGUA_PEDRA_AZUL':
-          enterprise = []
           enterprise.push(data.$AGUA_PEDRA_AZUL_1)
           enterprise.push(data.$AGUA_PEDRA_AZUL_2)
           break
         case '$ALMIR_LAZARI':
-          enterprise = []
           enterprise.push(data.$ALMIR_LAZARI)
           break
         case '$AM_GRANITOS':
-          enterprise = []
           enterprise.push(data.$AM_GRANITOS)
+          break
+        case '$AMAGRAN':
+          enterprise.push(data.$AMAGRAN_1)
+          enterprise.push(data.$AMAGRAN_2)
+          enterprise.push(data.$AMAGRAN_3)
+          enterprise.push(data.$AMAGRAN_4)
+          enterprise.push(data.$AMAGRAN_5)
+          break
+        case '$AREPEDRA':
+          enterprise.push(data.$AREPEDRA)
+          break
+        case '$BELA_VISTA':
+          enterprise.push(data.$BELA_VISTA)
+          break
+        case '$BELLACER':
+          enterprise.push(data.$BELLACER)
+          break
+        case '$BIBOM':
+          enterprise.push(data.$BIBOM)
+          break
+        case '$BRASIF':
+          enterprise.push(data.$BRASIF)
+          break
+        case '$CAMAGRAN':
+          enterprise.push(data.$CAMAGRAN)
+          break
+        case '$MINERAÇÃO_CAMPINHO':
+          enterprise.push(data.$MINERAÇÃO_CAMPINHO)
+          break
+        case '$MINERAÇÃO_COLODETT':
+          enterprise.push(data.$MINERAÇÃO_COLODETT)
+          break
+        case '$CONSTRUBLOK':
+          enterprise.push(data.$CONSTRUBLOK)
+          break
+        case '$CONSTRACTOR':
+          enterprise.push(data.$CONSTRACTOR)
           break
         default:
           enterprise = []
